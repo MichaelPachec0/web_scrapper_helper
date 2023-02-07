8b88e8ba13f5edf767cc173862b80fce616e52a2
Michael Pacheco <git@michaelpacheco.org>
2023-02-06 17:02:43 -0800

    webscrapper:cookies: refactor build cookie
    This is now compatible with cookiestore. This means that clients such as reqwest and others that use cookie stores will benefit from this change.
    * build_cookies_url returns a iterator with a tuple of Cookie and Url. Makes easy use in cookie stores insert_raw_cookie.
    * There are struct_to_raw_cookie and raw_cookie_to_struct that help conversion from either formats. Allows for easy export that the the export_cookie function takes advantage of.
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #![allow(
     clippy::missing_docs_in_private_items,
     clippy::missing_errors_doc,
@@ -24,64 +23,102 @@
 mod schema;
 
 pub mod scrapper_cookie {
+
     pub use crate::schema::CookieStruct;
     use conversions_rust_lib::ErrToLibErr;
     pub use cookie::Cookie as RawCookie;
     use cookie::SameSite;
     use std::fs::File;
-    use std::io::{BufWriter, Write};
+    use std::io::{BufReader, BufWriter, Write};
     use std::ops::Add;
     use time::{Duration, OffsetDateTime};
+    use url::Url;
 
-    // accepts a json with a top level array of cookie objects (represented by the CookieStruct
-    //  in the schema file)
-    #[inline]
-    pub fn build_cookie(raw_str: &str, debug: bool) -> Result<Vec<RawCookie>, liberr::Err> {
-        let cookie_vec = serde_json::from_str::<Vec<CookieStruct>>(raw_str)
-            .map_err(|err| liberr::Err::new(err.to_string(), line!()))?;
-        let ret = cookie_vec
+    pub(crate) fn build_cookie(raw_str: &str) -> Result<Vec<RawCookie>, liberr::Err> {
+        Ok(str_to_json(raw_str)
+            .err_to_lib_err(line!())?
             .into_iter()
-            .filter_map(|raw| {
-                let time = match raw.expires {
-                    -1 => cookie::Expiration::Session,
-                    time => match OffsetDateTime::from_unix_timestamp(time) {
-                        Ok(datetime) => cookie::Expiration::DateTime(datetime),
-                        _ => cookie::Expiration::Session,
-                    },
-                };
-                if let cookie::Expiration::DateTime(dtime) = time {
-                    // TODO: Decide if an offset is needed, and if so, for how long, for now default to
-                    //  an hour.
-                    if (dtime + Duration::hours(1)) < OffsetDateTime::now_utc() {
-                        if debug {
-                            println!("EXPIRED COOKIE: {raw:?} WITH DATE {}", dtime.to_string());
-                        }
-                        return None;
-                    }
-                }
-                Some(
-                    RawCookie::build(raw.name, raw.value)
-                        .domain(raw.domain)
-                        .path(raw.path)
-                        .secure(raw.secure)
-                        .http_only(raw.httpOnly)
-                        .expires(time)
-                        .same_site(match raw.sameSite.as_str() {
-                            "Strict" => SameSite::Strict,
-                            "Lax" => SameSite::Lax,
-                            _ => SameSite::None,
-                        })
-                        .finish(),
-                )
-            })
-            .collect::<Vec<RawCookie>>();
-        Ok(ret)
+            .filter_map(struct_to_raw_cookie_chain)
+            .collect::<Vec<RawCookie>>())
     }
 
-    // converts a formatted cookie struct to the intermediary custom struct so that it can be written
-    //  to a file
+    /// Builds an Iterator with a RawCookie, Url tuple, for use in insert raw cookie from cookie store
+    pub fn build_cookies_url(raw_str: &str) -> impl Iterator<Item = (RawCookie<'_>, Url)> {
+        // For now silently fail, may decide later to bubble up these errors later.
+        build_raw_cookie_chained(raw_str).filter_map(|cookie| {
+            let str = cookie.domain()?;
+            let url = format!("https://{str}");
+            let url = Url::parse(&url).ok()?;
+            Some((cookie, url))
+        })
+    }
+
+    /// accepts a json with a top level array of cookie objects (represented by the CookieStruct
+    ///  in the schema file) and converts to a RawCookie
+    /// # Panics
+    ///
+    /// Will panic if the str is either invalid json or if it is valid, does not follow the schema in schema.rs
+    pub fn build_raw_cookie_chained(raw_str: &str) -> Box<dyn Iterator<Item = RawCookie<'_>> + '_> {
+        match build_raw_cookie(raw_str) {
+            Ok(x) => Box::new(x),
+            Err(_) => Box::new(core::iter::empty::<RawCookie>()),
+        }
+    }
+    pub fn build_raw_cookie(
+        raw_str: &str,
+    ) -> Result<impl Iterator<Item = RawCookie<'_>>, liberr::Err> {
+        Ok(str_to_json(raw_str)
+            .err_to_lib_err(line!())?
+            .into_iter()
+            .filter_map(struct_to_raw_cookie_chain))
+    }
+
+    fn str_to_json(raw_str: &str) -> Result<Vec<CookieStruct>, liberr::Err> {
+        serde_json::from_str::<Vec<CookieStruct>>(raw_str).err_to_lib_err(line!())
+    }
+
+    fn struct_to_raw_cookie_chain<'a>(raw: CookieStruct) -> Option<RawCookie<'a>> {
+        struct_to_raw_cookie(raw, false)
+    }
+
+    fn struct_to_raw_cookie<'a>(raw: CookieStruct, debug: bool) -> Option<RawCookie<'a>> {
+        let time = match raw.expires {
+            -1 => cookie::Expiration::Session,
+            time => match OffsetDateTime::from_unix_timestamp(time) {
+                Ok(datetime) => cookie::Expiration::DateTime(datetime),
+                _ => cookie::Expiration::Session,
+            },
+        };
+        if let cookie::Expiration::DateTime(dtime) = time {
+            // TODO: Decide if an offset is needed, and if so, for how long, for now default to
+            //  an hour.(dtime + Duration::hours(1))
+            if dtime.checked_add(Duration::hours(1))? < OffsetDateTime::now_utc() {
+                if debug {
+                    println!("EXPIRED COOKIE: {raw:?} WITH DATE {dtime}");
+                }
+                return None;
+            }
+        }
+        Some(
+            RawCookie::build(raw.name, raw.value)
+                .domain(raw.domain)
+                .path(raw.path)
+                .secure(raw.secure)
+                .http_only(raw.httpOnly)
+                .expires(time)
+                .same_site(match raw.sameSite.as_str() {
+                    "Strict" => SameSite::Strict,
+                    "Lax" => SameSite::Lax,
+                    _ => SameSite::None,
+                })
+                .finish(),
+        )
+    }
+
+    /// converts a formatted cookie struct to the intermediary custom struct so that it can be written
+    ///  to a file
     #[inline]
-    pub fn cookie_to_struct(cookie: &RawCookie) -> CookieStruct {
+    pub fn cookie_to_struct<'b>(cookie: &'b RawCookie<'b>) -> CookieStruct {
         CookieStruct {
             name: cookie.name().to_owned(),
             value: cookie.value().to_owned(),
